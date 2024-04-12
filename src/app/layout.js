53305f8ed375cfa5b7d199bc1babf204c9c838dc
wlsooo <kwonjisoo14@gmail.com>
2024-04-12 15:08:45 +0900

    nextjs bootstrap icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,15 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <header>
           <nav className="navbar">
