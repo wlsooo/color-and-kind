7d01d6302970372d716f5248e2a63e801a3b0b51
wlsooo <kwonjisoo14@gmail.com>
2024-04-12 10:34:15 +0900

    navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,14 +6,28 @@ export default function RootLayout({ children }) {
     <html>
       <body>
         <header>
-          <menu className="hello">
+          <nav className="navbar">
+              <a href="index.html" className="navbar_logo">c&k</a>
+
+              <ul className="navbar_menu">
+                  <li className="nav-item"><a href="explain.html" className="nav-link">색각이상 정보</a></li>
+                  <li className="nav-item"><a href="test.html" className="nav-link">색각이상 검사</a></li>
+                  <li className="nav-item"><a href="colorPicker.html" className="nav-link">이미지 색 추출</a></li>
+              </ul>
+
+              <div className="nav_toggleBtn" id="nav_toggle">
+                  <i className="fas fa-bars"></i>
+              </div>
+          </nav>
+          {/* <menu classNameName="navbar">
             <li><Link href="/color">메뉴1</Link></li>
             <li><Link href="/test">메뉴2</Link></li>
-          </menu>
+          </menu> */}
         </header>
         <div>{children}</div>
         <footer>Footer</footer>
       </body>
     </html>
+
   );
 }
